5551b686e0a5cfc0386f71f5799d536fedc095de
Kevin Hendrawan <projectkevin97@gmail.com>
2025-10-04 10:36:23 +0700

    fix:data

--- src/repositories/DigimonRepository.js
+++ src/repositories/DigimonRepository.js
@@ -1,14 +1,14 @@
 const { mysqlDB, mssqlDB } = require("../config/database");
 const logger = require("../utils/logger");
 
-
 class DigimonRepository {
     async loadStatus() {
         try {
             const pool = await mssqlDB();
-            const [rows] = await pool.query('SELECT TOP 1 * FROM [digital-monitoring].[vw_status]');
+            const result = await pool.request()
+                .query("SELECT TOP 1 * FROM [digital-monitoring].[vw_status]");
 
-            return rows.length > 0 ? rows[0] : null;
+            return result.recordset.length > 0 ? result.recordset[0] : null;
         } catch (err) {
             logger.error(`DigimonRepository Error: ${err.message}`);
             throw err;
@@ -18,9 +18,10 @@ class DigimonRepository {
     async loadResStat() {
         try {
             const pool = await mssqlDB();
-            const [rows] = await pool.query('SELECT TOP 1 stStatus FROM [digital-monitoring].[tblt_statussumary] WHERE stId = 1');
+            const result = await pool.request()
+                .query("SELECT TOP 1 stStatus FROM [digital-monitoring].[tblt_statussumary] WHERE stId = 1");
 
-            return rows.length > 0 ? rows[0] : null;
+            return result.recordset.length > 0 ? result.recordset[0] : null;
         } catch (err) {
             logger.error(`DigimonRepository Error: ${err.message}`);
             throw err;
@@ -30,9 +31,10 @@ class DigimonRepository {
     async loadSummary() {
         try {
             const pool = await mssqlDB();
-            const [rows] = await pool.query('SELECT * FROM [digital-monitoring].[vw_sumary]');
+            const result = await pool.request()
+                .query("SELECT * FROM [digital-monitoring].[vw_sumary]");
 
-            return rows.length > 0 ? rows : null;
+            return result.recordset.length > 0 ? result.recordset : null;
         } catch (err) {
             logger.error(`DigimonRepository Error: ${err.message}`);
             throw err;
@@ -40,4 +42,4 @@ class DigimonRepository {
     }
 }
 
-module.exports = DigimonRepository
+module.exports = DigimonRepository;
